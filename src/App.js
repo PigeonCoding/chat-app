ae52fc4d73747f58b1dc5eb265019f3e8acf048a
magmagaming001 <wassimdzd@gmail.com>
2022-08-20 16:56:34 +0100

    some ui i dunno

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import './App.css';
+import axios from 'axios';
+import React, {useState} from "react"
+import { v4 as uuid } from 'uuid';
+
+
+let ran = true
+let logged = false
+let waslogged = false
+
+
+const adress = "http://localhost:8000"
+const client = axios.create({
+  baseURL: adress
+});
+
+
+export default App;
 
 function App() {
+
+
+  const [users, setUsers] = useState([])
+
+  if(ran){client.get("/users").then((res) => {setUsers(res.data);ran=false})}
+
+
   return (
     <div className="App">
-      <p>hello world</p>
+      <Loginn users={users} />
     </div>
   );
 }
 
-export default App;
+function Loginn(props) {
+  const [hmmm, sethmmm] = useState("")
+  const [fuser, setUser] = useState({
+    name: "",
+    passwd: "",
+  })
+
+  function unloggin(){
+    sethmmm("")
+    logged = false
+  }
+
+  return (
+    <div>
+      <form >
+        <label>
+          name: <input type={"text"} name="name" value={fuser.name} onChange={(e) => {setUser({...fuser, [e.target.name]: e.target.value})}} />
+          <br/>
+          passwd: <input type={"password"} name="passwd" value={fuser.passwd} onChange={(e) => {setUser({...fuser, [e.target.name]: e.target.value})}} />
+        </label>
+      </form>
+      <button onClick={(e) => {checkLogin(fuser, props.users,sethmmm);logged = true;checkLogin(fuser, props.users,sethmmm)}} >login</button>
+      {logged ? <br/> : <br/>}
+      {!logged ? null : <button onClick={(e) => {unloggin()}} >hello</button>}
+      {!logged ? null : <h2>{hmmm}</h2>}
+    </div>
+  )
+}
+
+function checkLogin(user, users, sethmmm){
+  users.forEach(element => {
+    console.log(logged)
+    if(user.name === element.name && user.passwd === element.passwd && logged ){
+      sethmmm("success")
+    }
+  });
+}
+
